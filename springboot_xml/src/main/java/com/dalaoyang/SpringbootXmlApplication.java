a1c1f464d1c0eaacf04110a16d747a04f152367a
zhoumanying <2837590939@qq.com>
2018-11-01 14:28:51 +0800

    提交Springboot的学习资料

---
+++ springboot_xml/src/main/java/com/dalaoyang/SpringbootXmlApplication.java
@@ -0,0 +1,18 @@
+package com.dalaoyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class SpringbootXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootXmlApplication.class, args);
+	}
+
+}
